a179f037bc1d234ea58d0f3497eb5fdd9437b230
extf8 <extfield8@gmail.com>
2022-09-04 12:59:33 +0200

    Added functions subtract multiply divide
    Added variables for num1 num2

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+let num1 = Number(document.getElementById('firstNumber').value);
+let num2 = Number(document.getElementById('secondNumber').value);
+let result = document.getElementById('result');
+
 const add = function() {
-    num1 = Number(document.getElementById('firstNumber').value);
-    num2 = Number(document.getElementById('secondNumber').value);
-    document.getElementById('result').innerText = num1 + num2;
+    return result.innerText = num1 + num2;
 }
 
+const subtract = function() {
+    return result.innerText = num1 - num2;
+}
+
+const multiply = function() {
+    return result.innerText = num1 * num2;
+}
+
+const divide = function() {
+    return result.innerText = num1 / num2;
+}
