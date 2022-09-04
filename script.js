1adbce852c28a39aebaba60ca1dc1612636069c6
extf8 <extfield8@gmail.com>
2022-09-04 12:43:30 +0200

    Added function add

---
+++ script.js
@@ -0,0 +1,6 @@
+const add = function() {
+    num1 = Number(document.getElementById('firstNumber').value);
+    num2 = Number(document.getElementById('secondNumber').value);
+    document.getElementById('result').innerText = num1 + num2;
+}
+
