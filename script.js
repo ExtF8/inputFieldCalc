bebba7afaa4b11bdd7fa0631e11c00f37950f89b
extf8 <extfield8@gmail.com>
2022-11-13 09:26:50 +0100

    Added event listeners Removed in html reset btn

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-let num1 = Number(document.getElementById('firstNumber').value);
-let num2 = Number(document.getElementById('secondNumber').value);
+
+let inputTextValue1 = document.getElementById('firstNumber');
+let inputTextValue2 = document.getElementById('secondNumber');
 let result = document.getElementById('result');
+
+inputTextValue1.addEventListener("change", (e) => {
+    num1 = Number(e.target.value)
+    console.log(num1)
+    return num1
+})
+
+inputTextValue2.addEventListener("change", (e) => {
+    num2 = Number(e.target.value)
+    console.log(num2)
+    return num2
+})
+
 
 add = () => {
     return result.innerText = num1 + num2;
@@ -13,8 +27,8 @@ subtract = () => {
 // multiply without * , with cycle - for/while/
 
 multiply = () => {
-
     let results = 0;
+
     for (let i = 0; i < num2; i++) {
         results = results + num1;
     }
