d281af0a4e8b1ee685fbfbb8fafe0b416048d067
extf8 <extfield8@gmail.com>
2022-11-27 15:41:30 +0100

    Gizmo refactor tractor

--- script.js
+++ script.js
@@ -1,45 +1,75 @@
-
 let inputTextValue1 = document.getElementById('firstNumber');
 let inputTextValue2 = document.getElementById('secondNumber');
 let result = document.getElementById('result');
+let num1 = 0;
+let num2 = 0;
+
+window.onload = () => {
+    num1 = getValue1();
+    num2 = getValue2();
+}
 
 inputTextValue1.addEventListener("change", (e) => {
-    num1 = Number(e.target.value)
-    console.log(num1)
-    return num1
+    num1 = getValue1();
+    console.log(num1);
 })
 
 inputTextValue2.addEventListener("change", (e) => {
-    num2 = Number(e.target.value)
-    console.log(num2)
-    return num2
+    num2 = getValue2();
+    console.log(num2);
 })
 
+getValue1 = () => {
+    return Number(inputTextValue1.value);
+}
+
+getValue2 = () => {
+    return Number(inputTextValue2.value);
+}
+
+clearFields = () => {
+    inputTextValue1.value = '';
+    inputTextValue2.value = '';
+    result.innerText = '';
+}
 
 add = () => {
-    return result.innerText = num1 + num2;
+    result.innerText = num1 + num2;
 }
 
 subtract = () => {
-    return result.innerText = num1 - num2;
+    result.innerText = num1 - num2;
 }
 
 // multiply without * , with cycle - for/while/
 
 multiply = () => {
-    let results = 0;
+    let f = num1;
+    let i = num2;
 
-    for (let i = 0; i < num2; i++) {
-        results = results + num1;
+    ++i;
+    while(i < 4) {
+        f = f * i;
+        ++i;
     }
-    result.innerText = results;
+
+    console.log("i:", i, "f:", f);
 }
 
 // multiply = () => {
-//     return result.innerText = num1 * num2;
+//     let results = 0;
+
+//     for (let i = 0; i < num2; i++) {
+//         results = results + num1;
+//     }
+//     result.innerText = results;
+// }
+
+// multiply = () => {
+//     result.innerText = num1 * num2;
 // }
 
 divide = () => {
-    return result.innerText = num1 / num2;
+    result.innerText = num1 / num2;
 }
 
